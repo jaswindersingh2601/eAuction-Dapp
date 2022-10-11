5b42ae70d055f938c7dd42818476a4840474053c
jaswindersingh2601 <jassighuman591@gmail.com>
2022-10-11 18:07:28 +0530

    Sell page created

--- client/src/pages/Auction.jsx
+++ client/src/pages/Auction.jsx
@@ -1,7 +1,15 @@
 export default function Auction(){
     return(
         <>
-        Auction
+            <div className="auction">
+                <h1>Auctions</h1>
+                <div className="ongoingAuction">
+                    <h2>Ongoing Auction</h2>
+                </div>
+                <div className="upcomingAuction">
+                    <h2>Upcoming Auction</h2>
+                </div>
+            </div> 
         </>
     )
 }
--- client/src/pages/Sell.jsx
+++ client/src/pages/Sell.jsx
@@ -1,7 +1,30 @@
+import '../css/Sell.css';
+
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 export default function Sell(){
+
     return(
-        <>
-        Sell
+        <>  
+          <div className="form-container">
+            <form action='../../server/post' method='post'>
+                <div>
+                    <input type="text" placeholder="Product Name"/>
+                </div>
+                <div>
+                    <textarea placeholder="Product Description" rows="4" cols="74">
+
+                    </textarea>
+                </div>
+                <div>
+                    <input type="text" name="" id="" placeholder="Starting Bid"/>
+                </div>
+                <div>
+                    <input type="Submit" value="Create Auction"/>
+                </div>
+            </form>
+          </div>
         </>
     )
 }
